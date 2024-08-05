eb70e5f2faba3959635d6dcd15e55bbc75f4e254
jiafangtao <jiafangtao@gmail.com>
2024-08-05 18:12:11 +0800

    hook up with mysql db

--- dal/dal.go
+++ dal/dal.go
@@ -3,7 +3,6 @@ package dal
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"database/sql"
 
@@ -17,11 +16,11 @@ var db *sql.DB
 func Connect() error {
 	// Capture connection properties.
 	cfg := mysql.Config{
-		User:   os.Getenv("DBUSER"), //TODO
-		Passwd: os.Getenv("DBPASS"), //TODO
+		User:   "root", //TODO
+		Passwd: "root", //TODO
 		Net:    "tcp",
-		Addr:   "127.0.0.1:3306", //TODO
-		DBName: "recordings",     //TODO
+		Addr:   "127.0.0.1:3306",
+		DBName: "showcases",
 	}
 	// Get a database handle.
 	var err error
@@ -58,11 +57,12 @@ func QueryAllBooks() ([]model.Book, error) {
 	// Loop through rows, using Scan to assign column data to struct fields.
 	for rows.Next() {
 		var book model.Book
-		if err := rows.Scan(&book.Id, &book.Title, &book.AuthorId, &book.Description); err != nil {
+		if err := rows.Scan(&book.Id, &book.Title, &book.Description); err != nil {
 			return nil, fmt.Errorf("queryAllBooks: %v", err)
 		}
 		books = append(books, book)
 	}
+
 	if err := rows.Err(); err != nil {
 		return nil, fmt.Errorf("queryAllBooks: %v", err)
 	}
--- routes/books.go
+++ routes/books.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jiafangtao/showcases/dal"
 	"github.com/jiafangtao/showcases/model"
 )
 
@@ -12,6 +14,19 @@ func addBooksRoute(rg *gin.RouterGroup) {
 	var books []model.Book
 	ping := rg.Group("/books")
 	ping.GET("/", func(c *gin.Context) {
+
+		err := dal.Connect()
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		books, err = dal.QueryAllBooks()
+		fmt.Println("books: ", len(books))
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
 
 		c.JSON(http.StatusOK, gin.H{
 			"books": books,
